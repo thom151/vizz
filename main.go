a40debd2e1f18be6bcb4a8da928f8cb9ee231dc3
thom151 <thomassantos2003@gmail.com>
2024-12-30 15:35:20 +1100

    Welcome to Vizz

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello World")
+
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	handler := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/", handler)
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
